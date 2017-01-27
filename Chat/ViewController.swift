afb2a1775c703eca7be6a1932eb6a9d3e93f0844
jackson Gold <jacksongold@jacksons-macbook-air.local>
2017-01-27 09:51:57 -0800

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by jackson Gold on 1/27/17.
+//  Copyright © 2017 Infinite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
